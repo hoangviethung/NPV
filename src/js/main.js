d21d4030b07cc805af86834efb0dfc408aeb0160
hoangviethung <hoangviet.hung.009@gmail.com>
2019-09-21 11:07:09 +0700

    DONE HOME BANNER

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,90 @@
-// Call functions here
-document.addEventListener("DOMContentLoaded", () => {
-	objectFitImages('.obj-fit-cover');
+// Function thêm class lazyload vào các thẻ <img> có thuộc tính [data-src]
+const addClassLazyload = () => {
+	let imgList = document.querySelectorAll("img[data-src]")
+	Array.prototype.forEach.call(imgList, function(el) {
+		if (el.className.length > 0) {
+			el.className = el.className + " lazyload"
+		} else {
+			el.className = "lazyload"
+		}
+	});
+}
+
+// Script cho tab
+class Tab {
+	selector;
+	titleList;
+	contentList;
+
+	constructor(selector) {
+		this.selector = document.querySelector(selector);
+		if (this.selector) {
+			this.titleList = this.selector.querySelectorAll("[toggle-for]")
+			this.contentList = this.selector.querySelectorAll("[tab-id]")
+			this.init();
+		}
+	}
+
+	runTabWhenClicked() {
+		Array.prototype.forEach.call(this.titleList, (element, index) => {
+			element.addEventListener("click", e => {
+				e.preventDefault();
+				const tabTarget = element.attributes["toggle-for"].value;
+				const targetDOM = this.selector.querySelector(`[tab-id='${tabTarget}']`);
+				element.classList.add("active");
+				Array.prototype.forEach.call(this.titleList, (eleClicked, eleClickedIndex) => {
+					if (eleClickedIndex != index) {
+						eleClicked.classList.remove("active")
+					}
+				});
+				Array.prototype.forEach.call(this.contentList, (tabContentElement) => {
+					if (tabContentElement.attributes["tab-id"].value != tabTarget) {
+						tabContentElement.style.display = "none"
+						tabContentElement.classList.remove("show")
+					}
+				});
+				targetDOM.style.display = "block"
+				setTimeout(() => {
+					targetDOM.classList.add("show")
+				}, 50);
+			})
+		})
+	}
+
+	activeFirstTab() {
+		this.titleList[0].click();
+	}
+
+	init() {
+		this.runTabWhenClicked();
+		this.activeFirstTab();
+	}
+}
+
+function sliderHomeBanner() {
+	var swpier = new Swiper('.slider-HomeBanner', {
+		effect: 'fade',
+		fadeEffect: {
+			crossFade: true,
+		},
+		centeredSlides: true,
+		speed: 1000,
+		loop: true,
+		autoplay: true,
+		breakpoints: {},
+		navigation: {
+			nextEl: '.slider-HomeBanner .swiper-button-next',
+			prevEl: '.slider-HomeBanner .swiper-button-prev',
+		},
+	});
+}
+
+
+$(document).ready(function() {
+	objectFitImages("img.ofc");
+	sliderHomeBanner();
+})
+
+window.addEventListener("scroll", () => {
+	addClassHeaderWhenScroll();
 })
