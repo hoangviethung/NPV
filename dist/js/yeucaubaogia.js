fd55fe1fe308fefa7f6f5c6fc62e125731c8b00e
Vũ Sơn <sonvh2510@gmail.com>
2019-10-28 12:16:11 +0700

    update lại script tính báo giá

--- dist/js/yeucaubaogia.js
+++ dist/js/yeucaubaogia.js
@@ -1,186 +1,139 @@
 const YeuCauBaoGia = () => {
+	const hiddenKichThuoc = document.querySelector('.box-hiden-dai-rong-cao');
+	const hiddenSoLuong = document.querySelector('.box-so-luong');
+	const hiddenTrongLuong = document.querySelector('.box-trong-luong');
+	const body = document.querySelector('.yeu-cau-bao-gia-table table tbody');
+	const rowSum = body.querySelector('.table-tong-cong');
 
-	const textareaKichThuoc = document.querySelector('.box-hiden-dai-rong-cao');
-	const textareaSoLuong = document.querySelector('.box-so-luong');
-	const textareaTrongLuong = document.querySelector('.box-trong-luong');
-	const bodyTableBaoGia = document.querySelector('.yeu-cau-bao-gia-table table tbody');
-
-	const getDataPerRow = row => {
-		let rowData = {};
-		if (row.querySelector('.dai').value) {
-			rowData.dai = row.querySelector('.dai').value
-		} else {
-			rowData.dai = 0;
-		}
-		if (row.querySelector('.rong').value) {
-			rowData.rong = row.querySelector('.rong').value
-		} else {
-			rowData.rong = 0
-		}
-		if (row.querySelector('.cao').value) {
-			rowData.cao = row.querySelector('.cao').value
-		} else {
-			rowData.cao = 0
-		}
-		if (row.querySelector('.soluong').value) {
-			rowData.soLuong = row.querySelector('.soluong').value
-		} else {
-			rowData.soLuong = 0;
-		}
-		if (row.querySelector('.trongluong').value) {
-			rowData.trongLuong = row.querySelector('.trongluong').value
-		} else {
-			rowData.trongLuong = 0;
-		}
-		return rowData;
+	const getAllRow = () => {
+		return Array.from(body.querySelectorAll('.table-input'));
 	};
 
+	const getRowData = row => {
+		return {
+			dai: Number(row.querySelector('.dai').value),
+			rong: Number(row.querySelector('.rong').value),
+			cao: Number(row.querySelector('.cao').value),
+			soluong: Number(row.querySelector('.soluong').value),
+			trongluong: Number(row.querySelector('.trongluong').value),
+			isFull: !!(row.querySelector('.dai').value && row.querySelector('.rong').value && row.querySelector('.cao').value && row.querySelector('.soluong').value && row.querySelector('.trongluong').value),
+		};
+	};
 
-	const getAllDataToArea = (rowArray) => {
+	const getRowResult = (row, data) => {
+		const rusult_TheTich = (data.dai * data.rong * data.cao) / 1000000 * data.soluong;
+		const rusult_TrongluongAIR = (data.dai * data.rong * data.cao) / 6000 * data.soluong;
+		const rusult_TrongluongCourier = (data.dai * data.rong * data.cao) / 5000 * data.soluong;
+		const rusult_TrongLuong = data.soluong * data.trongluong;
+		row.querySelector('.tongthetich').value = rusult_TheTich;
+		row.querySelector('.tongtrongluong').value = rusult_TrongLuong;
+		if (rusult_TrongLuong > rusult_TrongluongAIR) {
+			row.querySelector('.tongtrongluongAIR').value = rusult_TrongLuong;
+		} else {
+			row.querySelector('.tongtrongluongAIR').value = rusult_TrongluongAIR;
+		}
+		if (rusult_TrongLuong > rusult_TrongluongCourier) {
+			row.querySelector('.tongtrongluongCourier').value = rusult_TrongLuong;
+		} else {
+			row.querySelector('.tongtrongluongCourier').value = rusult_TrongluongCourier;
+		}
+		addRow(data.isFull);
+	};
+
+	const setRowHandler = row => {
+		row.addEventListener('change', () => {
+			getRowResult(row, getRowData(row));
+		});
+	};
+
+	const setRowRemoveHandler = row => {
+		const btnRemove = row.querySelector('.remove input[type="button"]');
+		btnRemove.addEventListener('click', () => {
+			btnRemove.parentNode.parentNode.parentNode.removeChild(btnRemove.parentNode.parentNode);
+			UpdateDataEveryTableChange();
+		});
+	};
+
+	const addRow = check => {
+		if (check) {
+			const newRow = body.querySelector('.table-input').cloneNode(true);
+			Array.from(newRow.querySelectorAll('input[type="text"], input[type="number"]')).forEach(input => {
+				input.value = null;
+			});
+			newRow.querySelector('input[type="button"]').removeAttribute('disabled');
+			setRowHandler(newRow);
+			setRowRemoveHandler(newRow);
+			body.insertBefore(newRow, rowSum);
+		}
+	};
+
+	const UpdateDataEveryTableChange = () => {
+		// get sum result per column data
+		let SUM_soluong = 0;
+		let SUM_tongthetich = 0;
+		let SUM_tongtrongluong = 0;
+		let SUM_trongluongtinhcuocAir = 0;
+		let SUM_trongluongtinhcuocCourier = 0;
+
+		Array.from(body.querySelectorAll('.soluong')).forEach(item => {
+			SUM_soluong += Number(item.value);
+		});
+
+		Array.from(body.querySelectorAll('.tongthetich')).forEach(item => {
+			SUM_tongthetich += Number(item.value);
+		});
+
+		Array.from(body.querySelectorAll('.tongtrongluong')).forEach(item => {
+			SUM_tongtrongluong += Number(item.value);
+		});
+
+		Array.from(body.querySelectorAll('.tongtrongluongAIR')).forEach(item => {
+			SUM_trongluongtinhcuocAir += Number(item.value);
+		});
+
+		Array.from(body.querySelectorAll('.tongtrongluongCourier')).forEach(item => {
+			SUM_trongluongtinhcuocCourier += Number(item.value);
+		});
+
+		SUM_soluong = Math.round(SUM_soluong * 100) / 100;
+		SUM_tongthetich = Math.round(SUM_tongthetich * 100) / 100;
+		SUM_tongtrongluong = Math.round(SUM_tongtrongluong * 100) / 100;
+		SUM_trongluongtinhcuocAir = Math.round(SUM_trongluongtinhcuocAir * 100) / 100;
+		SUM_trongluongtinhcuocCourier = Math.round(SUM_trongluongtinhcuocCourier * 100) / 100;
+
+		// IN RA MÀN HÌNH GIÁ TRỊ TỔNG => LÀM TRÒN ĐẾN 2 CHỮ SÓ THẬP PHÂN CUỐI
+		document.querySelector('.SUM_soluong').value = SUM_soluong;
+		document.querySelector('.SUM_tongthetich').value = SUM_tongthetich;
+		document.querySelector('.SUM_tongtrongluong').value = SUM_tongtrongluong;
+		document.querySelector('.SUM_trongluongtinhcuocAir').value = SUM_trongluongtinhcuocAir;
+		document.querySelector('.SUM_trongluongtinhcuocCourier').value = SUM_trongluongtinhcuocCourier;
+
+
+		// Set list data to hidden textarea
 		let kichThuoc = '';
 		let soLuong = '';
 		let trongLuong = '';
-		rowArray.forEach(input => {
-			const rowData = getDataPerRow(input);
+		getAllRow().forEach(row => {
+			const rowData = getRowData(row);
 			kichThuoc += `${rowData.dai}x${rowData.rong}x${rowData.cao}\n`;
-			soLuong += `${rowData.soLuong}\n`;
-			trongLuong += `${rowData.trongLuong}\n`;
-		})
-
-		textareaKichThuoc.value = kichThuoc;
-		textareaSoLuong.value = soLuong;
-		textareaTrongLuong.value = trongLuong;
-	};
-
-	const calculateRow = (row) => {
-		const rowData = getDataPerRow(row);
-		const tongTheTich = (rowData.dai * rowData.rong * rowData.cao) / 1000000 * rowData.soLuong;
-		const tongtrongluongAIR = (rowData.dai * rowData.rong * rowData.cao) / 6000 * rowData.soLuong;
-		const tongtrongluongCourier = (rowData.dai * rowData.rong * rowData.cao) / 5000 * rowData.soLuong;
-		const tongTrongLuong = rowData.soLuong * rowData.trongLuong;
-
-		row.querySelector('.tongthetich').value = tongTheTich;
-		row.querySelector('.tongtrongluong').value = tongTrongLuong;
-
-		// SO SÁNH 2 CỘT AIR VÀ COURIER -> NẾU NHỎ HƠN CỘT TỔNG TRỌNG LƯỢNG THÌ LẤY DATA CỦA TỔNG TRỌNG LƯỢNG
-		if (tongTrongLuong > tongtrongluongAIR) {
-			row.querySelector('.tongtrongluongAIR').value = tongTrongLuong;
-		} else if (tongTrongLuong > tongtrongluongCourier) {
-			row.querySelector('.tongtrongluongCourier').value = tongTrongLuong;
-		} else {
-			row.querySelector('.tongtrongluongAIR').value = tongtrongluongAIR;
-			row.querySelector('.tongtrongluongCourier').value = tongtrongluongCourier;
-		}
-
-		if (rowData.dai && rowData.rong && rowData.cao && rowData.soLuong && rowData.trongLuong) {
-			const lastInputRow = document.querySelectorAll('.yeu-cau-bao-gia-table .table-input')[document.querySelectorAll('.yeu-cau-bao-gia-table .table-input').length - 1]
-			const valueLastRow = getDataPerRow(lastInputRow);
-			if (valueLastRow.dai && valueLastRow.rong && valueLastRow.cao && valueLastRow.soLuong && valueLastRow.trongLuong) {
-				return addRow(row);
-			}
-		}
-	};
-
-	const addRow = row => {
-		const newRow = row.cloneNode(true);
-		bodyTableBaoGia.insertBefore(newRow, bodyTableBaoGia.querySelector('.table-tong-cong'));
-		Array.from(newRow.querySelectorAll('input[type="number"],input[type="text"]')).forEach(input => {
-			input.value = null;
-		})
-		newRow.querySelector('input[type="button"]').removeAttribute('disabled')
-		const rowArray = Array.from(document.querySelectorAll('.yeu-cau-bao-gia-table .table-input'));
-		Array.from(newRow.querySelectorAll('input')).forEach(input => {
-			input.addEventListener('change', () => {
-				calculateRow(newRow);
-				getAllDataToArea(rowArray);
-			})
-		})
-		const btn = newRow.querySelector('input[type="button"]')
-		btn.addEventListener('click', () => {
-			btn.parentNode.parentNode.parentNode.removeChild(btn.parentNode.parentNode);
-			const rowArray = Array.from(document.querySelectorAll('.yeu-cau-bao-gia-table .table-input'));
-			getAllDataToArea(rowArray);
-		})
-		tinhtong();
-	}
-
-	const removeRow = () => {
-		const removeButtonArray = Array.from(document.querySelectorAll('.yeu-cau-bao-gia-table .table-input .remove input'));
-		if (removeButtonArray) {
-			removeButtonArray.forEach(btn => {
-				btn.addEventListener('click', () => {
-					btn.parentNode.removeChild(btn);
-					const rowArray = Array.from(document.querySelectorAll('.yeu-cau-bao-gia-table .table-input'));
-					getAllDataToArea(rowArray);
-				})
-			})
-		}
-		tinhtong();
-	}
-
-	const run = () => {
-		const rowArray = Array.from(document.querySelectorAll('.yeu-cau-bao-gia-table .table-input'));
-		if (rowArray) {
-			rowArray.forEach(row => {
-				Array.from(row.querySelectorAll('input')).forEach(input => {
-					input.addEventListener('change', () => {
-						calculateRow(row);
-						getAllDataToArea(rowArray);
-						tinhtong(row);
-					})
-				})
-			})
-		}
-	}
-
-	const tinhtong = () => {
-		$('.yeu-cau-bao-gia-table input[type="number"]').on('change', function (e) {
-			let SUM_soluong = 0;
-			let SUM_trongluong = 0;
-			let SUM_tongthetich = 0;
-			let SUM_tongtrongluong = 0;
-			let SUM_trongluongtinhcuocAir = 0;
-			let SUM_trongluongtinhcuocCourier = 0;
-
-			$('.soluong').each(function () {
-				SUM_soluong += Number($(this).val());
-			})
-
-			$('.trongluong').each(function () {
-				SUM_trongluong += Number($(this).val());
-			})
-
-			$('.tongthetich').each(function () {
-				SUM_tongthetich += Number($(this).val());
-			})
-
-			$('.tongtrongluong').each(function () {
-				SUM_tongtrongluong += Number($(this).val());
-			})
-
-			$('.tongtrongluongAIR').each(function () {
-				SUM_trongluongtinhcuocAir += Number($(this).val());
-			})
-
-			$('.tongtrongluongCourier').each(function () {
-				SUM_trongluongtinhcuocCourier += Number($(this).val());
-			})
-
-			// IN RA MÀN HÌNH GIÁ TRỊ TỔNG => LÀM TRÒN ĐẾN 2 CHỮ SÓ THẬP PHÂN CUỐI
-			$('.SUM_soluong').val(Math.ceil(SUM_soluong));
-			// $('.SUM_trongluong').val(SUM_trongluong);
-			$('.SUM_tongthetich').val(Math.round(SUM_tongthetich * 100 + Number.EPSILON) / 100);
-			$('.SUM_tongtrongluong').val(Math.round(SUM_tongtrongluong * 100 + Number.EPSILON) / 100);
-			$('.SUM_trongluongtinhcuocAir').val(Math.round(SUM_trongluongtinhcuocAir * 100 + Number.EPSILON) / 100);
-			$('.SUM_trongluongtinhcuocCourier').val(Math.round(SUM_trongluongtinhcuocCourier * 100 + Number.EPSILON) / 100);
+			soLuong += `${rowData.soluong}\n`;
+			trongLuong += `${rowData.trongluong}\n`;
 		});
-	}
+		hiddenKichThuoc.value = kichThuoc;
+		hiddenSoLuong.value = soLuong;
+		hiddenTrongLuong.value = trongLuong;
+	};
 
-	// init
 	(() => {
-		run();
-		removeRow();
+		getAllRow().forEach(row => {
+			setRowHandler(row);
+			setRowRemoveHandler(row);
+		});
+
+		body.addEventListener('change', () => {
+			UpdateDataEveryTableChange();
+		})
 	})();
-}
+};
 
 module.exports = YeuCauBaoGia;
