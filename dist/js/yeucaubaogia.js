80109daaf946e6a81f490e2e7beb0fb5cd2caccf
hoangviethung <hoangviet.hung.009@gmail.com>
2020-05-04 15:55:45 +0700

    thêm 1 cột thế tích / kiện hàng

--- dist/js/yeucaubaogia.js
+++ dist/js/yeucaubaogia.js
@@ -25,8 +25,10 @@ const YeuCauBaoGia = () => {
 		const rusult_TrongluongAIR = (data.dai * data.rong * data.cao) / 6000 * data.soluong;
 		const rusult_TrongluongCourier = (data.dai * data.rong * data.cao) / 5000 * data.soluong;
 		const rusult_TrongLuong = data.soluong * data.trongluong;
+		const rusult_thetich__1kienhang = (data.dai * data.rong * data.cao) / 1000000;
 		row.querySelector('.tongthetich').value = rusult_TheTich;
 		row.querySelector('.tongtrongluong').value = rusult_TrongLuong;
+		row.querySelector('.thetich__1kienhang').value = rusult_thetich__1kienhang;
 		if (rusult_TrongLuong > rusult_TrongluongAIR) {
 			row.querySelector('.tongtrongluongAIR').value = rusult_TrongLuong;
 		} else {
@@ -116,7 +118,6 @@ const YeuCauBaoGia = () => {
 		SUM_tongtrongluong = Math.round(SUM_tongtrongluong * 100) / 100;
 		SUM_trongluongtinhcuocAir = Math.round(SUM_trongluongtinhcuocAir * 100) / 100;
 		SUM_trongluongtinhcuocCourier = Math.round(SUM_trongluongtinhcuocCourier * 100) / 100;
-
 		// IN RA MÀN HÌNH GIÁ TRỊ TỔNG => LÀM TRÒN ĐẾN 2 CHỮ SÓ THẬP PHÂN CUỐI
 		document.querySelector('.SUM_soluong').value = SUM_soluong;
 		document.querySelector('.SUM_trongluongtinhcuocAir').value = SUM_trongluongtinhcuocAir;
